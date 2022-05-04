3e2ebf4aa1369346c5191500398e8896d7fdbc05
Sidakveer <sidakveersingh8@gmail.com>
2022-05-04 17:21:21 -0400

    finnish code

--- birthday_wisher/main.py
+++ birthday_wisher/main.py
@@ -4,17 +4,16 @@ import datetime as dt
 import pandas
 
 TEMPLATES = ["letter_1.txt", "letter_2.txt", "letter_3.txt"]
-my_email = "ssidakveer@gmail.com"
-password = "!Kartar11"
+my_email = ""
+password = ""
 
 data = pandas.read_csv("birthdays.csv")
 dict_data = data.to_dict(orient="records")
 print(dict_data)
 
-# 2. Check if today matches a birthday in the birthdays.csv
 today = dt.datetime.now()
 for dictionary in dict_data:
-    if today.day == dictionary["day"]:
+    if today.day == dictionary["day"] and today.month == dictionary["month"]:
         letter_temp = random.choice(TEMPLATES)
         with open(f"letter_templates/{letter_temp}") as file1:
             data1 = file1.read()
@@ -24,4 +23,3 @@ for dictionary in dict_data:
             connection.starttls()
             connection.login(user=my_email, password=password)
             connection.sendmail(from_addr=my_email, to_addrs=dictionary["email"], msg=data1)
-            
