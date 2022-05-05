2527911b3665bce6d0a3683e05751722cf7b2aae
Sidakveer <sidakveersingh8@gmail.com>
2022-05-05 14:58:46 -0400

    finish code #2

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Using various modules such as **smtplib, pandas, datetime and more.** to build i
 This project contains a program which automates the process of sending out emails to people in our .csv file on their birthdays. The program uses **pandas** 
 to work with the csv file and **smtplib** module to build a connection with the email servers and send out emails from the users email address. The **datetime** module helps to keep a track of the current 
 day and month to check if anyone from the guest list has a birthday or not.
+## iss_tracker
+This project uses the **requests** module to use different **API's** to get a hold of the iss satellite's current position which revolves the earth continously and get
--- iss_satellite_tracker/main.py
+++ iss_satellite_tracker/main.py
@@ -1,48 +1,53 @@
 import smtplib
 import time
+
 import requests
 from datetime import datetime
 
 MY_LAT = 45.501690 # Your latitude
 MY_LONG = -73.567253 # Your longitude
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
-
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
 
 
 def position_check():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+
     if (40 < iss_latitude < 50.5) and (-78 < iss_longitude < -68):
         return True
     else:
         return False
 
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-time_now = datetime.now()
-print(time_now.hour)
-print(sunrise)
+    time_now = datetime.now()
 
-username = ""
-password = ""
+    if sunset < time_now.hour < sunrise:
+        return True
+
 
 while True:
     time.sleep(60)
-    if position_check() and sunset < time_now.hour < sunrise:
+    username = ""
+    password = ""
+    if position_check() and is_night():
         with smtplib.SMTP("smtp.gmail.com", port=578) as connection:
             connection.starttls()
             connection.login(user=username, password=password)
