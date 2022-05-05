4001217829b92a7978f8afb7489f34389901d10d
Sidakveer <sidakveersingh8@gmail.com>
2022-05-05 14:37:28 -0400

    add starting code

---
+++ iss_satellite_tracker/main.py
@@ -0,0 +1,38 @@
+import requests
+from datetime import datetime
+
+MY_LAT = 45.501690 # Your latitude
+MY_LONG = -73.567253 # Your longitude
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
+
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
+
+#Your position is within +5 or -5 degrees of the ISS position.
+
+
+
+# def position_check():
+#     if  < MY_LAT
+
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+time_now = datetime.now()
+
+
+
+
