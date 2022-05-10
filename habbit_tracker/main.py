ab695c66d66573c35decec1817440d1003b331f6
Sidakveer <sidakveersingh8@gmail.com>
2022-05-10 13:23:00 -0400

    add starter code

---
+++ habbit_tracker/main.py
@@ -0,0 +1,36 @@
+import requests
+
+
+USERNAME = "sidak"
+TOKEN = "dfsfdsfds"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+response = requests.post(url=pixela_endpoint, json=user_params)
+print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph12",
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai"
+
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response1 = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+print(response1.text)
+
