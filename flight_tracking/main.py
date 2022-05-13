3b3ab5ddf4254e4bfc72958c36d931df8349f59d
Sidakveer <sidakveersingh8@gmail.com>
2022-05-13 17:36:16 -0400

    add code

--- flight_tracking/main.py
+++ flight_tracking/main.py
@@ -1,12 +1,20 @@
+import requests
+
 from notification_manager import NotificationManager
 from flight_data import FlightData
 from flight_search import FlightSearch
 from data_manager import DataManager
 
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
-
 
 data_manager1 = DataManager()
 sheet_data = data_manager1.get_destination_data()
-print(sheet_data)
-print(data_manager1.update_destination_codes())
+print(sheet_data[0])
+
+for x in range(len(sheet_data)):
+    if sheet_data[x]["iataCode"] == "":
+        flight_search1 = FlightSearch()
+        sheet_data[x]["iataCode"] = flight_search1.get_destination_code()
+
+    data_manager1.destination_data = sheet_data
+    data_manager1.update_destination_codes()
+    print(sheet_data)
