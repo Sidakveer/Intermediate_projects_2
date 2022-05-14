8d30f93ac254f56b3385e9460adf3e86995c09bb
Sidakveer <sidakveersingh8@gmail.com>
2022-05-14 13:40:16 -0400

    update project

---
+++ flight_tracking/flight_data.py
@@ -0,0 +1,11 @@
+class FlightData:
+
+    def __init__(self, price, origin_city, origin_airport,
+                 destination_city, destination_airport, out_date=None, return_date=None):
+        self.price = price
+        self.origin_city = origin_city
+        self.origin_airport = origin_airport
+        self.des_city = destination_city
+        self.des_airport = destination_airport
+        self.out_date = out_date
+        self.return_date = return_date
