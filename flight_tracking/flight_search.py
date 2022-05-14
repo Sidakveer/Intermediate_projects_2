f4b3d00b2d6b7dff1f99b11d1fe578a2c28a7f73
Sidakveer <sidakveersingh8@gmail.com>
2022-05-14 12:23:33 -0400

    update project

--- flight_tracking/flight_search.py
+++ flight_tracking/flight_search.py
@@ -1,10 +1,22 @@
 import requests
 
-TEQUILA_ENDPOINT = ""
-TEQUILA_API_KEY = ""
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com/locations/query"
+TEQUILA_API_KEY = "WOHB9UC1vPwzasp2J2txwn7pxSblWL1X"
 
 class FlightSearch:
 
-    def get_destination_code(self):
-        code = "Testing"
+    def get_destination_code(self, cityName):
+
+        headers = {
+            "apikey": TEQUILA_API_KEY
+        }
+
+        paramas = {
+            "term": cityName,
+            "location_types": "city"
+        }
+        response = requests.get(url=TEQUILA_ENDPOINT, params=paramas, headers=headers)
+        result = response.json()
+        code = result["locations"][0]["code"]
         return code
+
