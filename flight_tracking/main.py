0222213a898b1ef92e1b86d881be7b88e21c2b8b
Sidakveer <sidakveersingh8@gmail.com>
2022-05-14 13:40:33 -0400

    update project

--- flight_tracking/main.py
+++ flight_tracking/main.py
@@ -1,3 +1,5 @@
+import datetime
+
 import requests
 
 from notification_manager import NotificationManager
@@ -8,14 +10,21 @@ from data_manager import DataManager
 
 data_manager1 = DataManager()
 sheet_data = data_manager1.get_destination_data()
-print(sheet_data[0])
+flight_search1 = FlightSearch()
+
+ORIGIN_CITY_IATA = "YUL"
 
 for x in range(len(sheet_data)):
     if sheet_data[x]["iataCode"] == "":
-        flight_search1 = FlightSearch()
         city_name = sheet_data[x]["city"]
         sheet_data[x]["iataCode"] = flight_search1.get_destination_code(city_name)
 
     data_manager1.destination_data = sheet_data
     data_manager1.update_destination_codes()
-    print(sheet_data)
+
+    tomorrow = datetime.datetime.now() + datetime.timedelta(days=1)
+    sixmonths_from_now = datetime.datetime.now() + datetime.timedelta(days=(6 * 30))
+
+    flight = flight_search1.check_flights(ORIGIN_CITY_IATA,
+                                          sheet_data[x]["iataCode"],
+                                          tomorrow, sixmonths_from_now)
